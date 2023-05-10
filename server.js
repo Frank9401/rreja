6cd4ec6513bcceecb65b1b875e8c2fb3740ab7a2
Frank9401 <faxriddin0115@gmail.com>
2023-05-10 10:04:18 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Server boshlash");
 const express = require("express");
 const app = express() ;
 const http =require("http")
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err,data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user =JSON.parse(data)
+    }
+});
 
 // 1 express ga kirib kelyatgan malumotlarga bogliq bolgan kodlar yoziladi. KIRISH kodlari
 app.use(express.static("public"));
@@ -10,6 +20,7 @@ app.use(express.urlencoded({extended:true})); //html forumdan request qiladi
 
 //2 Session
 
+
 //3 BSSR -backend serverside render  VIEWS ga bogliq kodlar
 app.set("views", "views");
 app.set("view engine","ejs");
@@ -17,8 +28,13 @@ app.set("view engine","ejs");
 //4 routerlarga moljallangan  ,Routin codlar
 
 app.post("/create-item",(req,res)=>{
+    // console.log("")
     
 });
+
+app.get('/author', (req, res) => {
+    res.render("author", {user:user});
+})
 
 app.get("/", function(req,res){
     res.render('harid')
@@ -31,3 +47,4 @@ let PORT = 3000;
 server.listen(PORT , function(){
     console.log(`The server is running succesfully on port : ${PORT}`)
 })
+
