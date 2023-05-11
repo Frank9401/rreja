32b951c915043581e2f5533f6cc9aeeaaae82198
Frank9401 <faxriddin0115@gmail.com>
2023-05-11 20:24:38 +0900

    feat: 8-dars publishing

--- server.js
+++ server.js
@@ -11,30 +11,30 @@ fs.readFile("database/user.json", "utf-8", (err,data) => {
     } else {
         user =JSON.parse(data)
     }
-});
+});  
 
 // 1 express ga kirib kelyatgan malumotlarga bogliq bolgan kodlar yoziladi. KIRISH kodlari
 app.use(express.static("public"));
 app.use(express.json());// kirib kelyatgan json formatdagi data ni objectga ogiradi
 app.use(express.urlencoded({extended:true})); //html forumdan request qiladi 
 
-//2 Session 
+//2 Session  
 
  
 //3 BSSR -backend serverside render  VIEWS ga bogliq kodlar
 app.set("views", "views");
 app.set("view engine","ejs");
-
+ 
 //4 routerlarga moljallangan  ,Routin codlar 
    
 app.post("/create-item",(req,res)=>{
-    // console.log("")
+    // console.log("") 
     
-});
+}); 
 
-// app.get('/author', (req, res) => {
-//     res.render("author", {user:user});
-// })
+app.get('/author', (req, res) => {
+    res.render("author", {user:user});
+})
  
 app.get("/", function(req,res){
     res.render('harid')
--- train.js
+++ train.js
@@ -1,86 +1,3 @@
-console.log("Jack Ma maslaxatlari");
-const list = [
-    'yashi talaba buling',  //0-20
-    'tugri boshliq tanlang va kupro xato qiling',  //20-30
-    'uzingizga ishlashni boshlang',  //30-40
-    'siz kuchli bulgan narsalarni qiling', //40-50
-    'yoshlarga investitsia qiling',  //50-60
-    'endi dam oling, foydasi yuq endi',  //60
-];
-
-// CALLBACK functions
-function maslahatBering(a, callback) {
-    if(typeof a !== 'number') callback("insert a number", null);
-    else if(a <= 20) callback(null, list[0]);
-    else if(a > 20 && a <= 30) callback(null,list[1]);
-    else if(a > 30 && a <= 40) callback(null,list[2]);
-    else if(a > 40 && a <= 50) callback(null,list[3]);
-    else if(a > 50 && a <= 60) callback(null,list[4]);
-    else {
-            setTimeout(function() {
-                callback(null, list[5]);
-            }, 5000);
-            
-    
-    }
-}
-
-console.log('passed here 0');
-maslahatBering(55,(err, data) => {
-    if (err) console.log('ERROR:', err);
-    else {
-    console.log('javob:',data);
-    }
-});
-console.log('passed here 1');
-    
-
-
-// console.log('passed here 0');
-// maslahatBering(70, (err, data) =>{
-//     if (err) console.log("ERROR:", err);
-//     else {
-//         console.log(data);
-//     }
-// });
-// console.log("passed here 1");
-
-// maslahatBering(10, (err, data) => {
-//     if(err) console.log('ERROR:', err);
-//     else {
-//         console.log('javob:',data);
-//     }
-// });
-// console.log("passed here 1");
-
-
-
-// call via then/catch
-// console.log("passed here 0");
-// maslahatBering(25)
-// .then((data) => {
-    
-// console.log('javob:', data);
-// })
-// .catch(err => {
-//     console.log('ERROR', err);
-// });
-// console.log("passed here 1");
-
-
-
-//call via asyn/wait 
-// async function run() {
-//     let javob = await maslahatBering(20);
-//     console.log(javob);
-//     javob = await maslahatBering(31);
-//     console.log(javob);
-//     javob = await maslahatBering(41);
-//     console.log(javob);
-// }
-
-
-
 // console.log("Jack Ma maslaxatlari");
 // const list = [
 //     'yashi talaba buling',  //0-20
@@ -91,51 +8,7 @@ console.log('passed here 1');
 //     'endi dam oling, foydasi yuq endi',  //60
 // ];
 
-// //ASYNC funtions
-// async function maslahatBering(a) {
-//     if(typeof a !== 'number') throw new Error("insert a number");
-//     else if(a <= 20) return list[0];
-//     else if(a > 20 && a <= 30)  return list[1];
-//     else if(a > 30 && a <= 40)  return list[2];
-//     else if(a > 40 && a <= 50)  return list[3];
-//     else if(a > 50 && a <= 60)  return list[4];
-//     else {
-
-//         return new Promise((resolve, reject) => {
-
-//             setTimeout(() => {
-//                 resolve(list[5]);
-//             }, 5000);
-//         });
-       
-//     }
-    
-// }
-
-// async function run() {
-//     let javob = await maslahatBering(25);
-//     console.log(javob);
-//     javob = await maslahatBering(70);
-//     console.log(javob);
-//     javob = await maslahatBering(41);
-//     console.log(javob);
-// }
-//  run();
-
-
-
-
-// console.log("Jack Ma maslaxatlari");
-// const list = [
-//     'yashi talaba buling',  //0-20
-//     'tugri boshliq tanlang va kupro xato qiling',  //20-30
-//     'uzingizga ishlashni boshlang',  //30-40
-//     'siz kuchli bulgan narsalarni qiling', //40-50
-//     'yoshlarga investitsia qiling',  //50-60
-//     'endi dam oling, foydasi yuq endi',  //60
-// ];
-
-
+// // CALLBACK functions
 // function maslahatBering(a, callback) {
 //     if(typeof a !== 'number') callback("insert a number", null);
 //     else if(a <= 20) callback(null, list[0]);
@@ -144,19 +17,156 @@ console.log('passed here 1');
 //     else if(a > 40 && a <= 50) callback(null,list[3]);
 //     else if(a > 50 && a <= 60) callback(null,list[4]);
 //     else {
-//         setInterval(function() {
-//             callback(null, list[5]);
-//         }, 1000);
+//             setTimeout(function() {
+//                 callback(null, list[5]);
+//             }, 5000);
+            
     
 //     }
-    
 // }
 
 // console.log('passed here 0');
-// maslahatBering(70, (err, data) =>{
-//     if (err) console.log("ERROR:", err);
+// maslahatBering(55,(err, data) => {
+//     if (err) console.log('ERROR:', err);
 //     else {
-//         console.log(data);
+//     console.log('javob:',data);
 //     }
 // });
-// console.log("passed here 1");
+// console.log('passed here 1');
+    
+
+
+// // console.log('passed here 0');
+// // maslahatBering(70, (err, data) =>{
+// //     if (err) console.log("ERROR:", err);
+// //     else {
+// //         console.log(data);
+// //     }
+// // });
+// // console.log("passed here 1");
+
+// // maslahatBering(10, (err, data) => {
+// //     if(err) console.log('ERROR:', err);
+// //     else {
+// //         console.log('javob:',data);
+// //     }
+// // });
+// // console.log("passed here 1");
+
+
+
+// // call via then/catch
+// // console.log("passed here 0");
+// // maslahatBering(25)
+// // .then((data) => {
+    
+// // console.log('javob:', data);
+// // })
+// // .catch(err => {
+// //     console.log('ERROR', err);
+// // });
+// // console.log("passed here 1");
+
+
+
+// //call via asyn/wait 
+// // async function run() {
+// //     let javob = await maslahatBering(20);
+// //     console.log(javob);
+// //     javob = await maslahatBering(31);
+// //     console.log(javob);
+// //     javob = await maslahatBering(41);
+// //     console.log(javob);
+// // }
+
+
+
+// // console.log("Jack Ma maslaxatlari");
+// // const list = [
+// //     'yashi talaba buling',  //0-20
+// //     'tugri boshliq tanlang va kupro xato qiling',  //20-30
+// //     'uzingizga ishlashni boshlang',  //30-40
+// //     'siz kuchli bulgan narsalarni qiling', //40-50
+// //     'yoshlarga investitsia qiling',  //50-60
+// //     'endi dam oling, foydasi yuq endi',  //60
+// // ];
+
+// // //ASYNC funtions
+// // async function maslahatBering(a) {
+// //     if(typeof a !== 'number') throw new Error("insert a number");
+// //     else if(a <= 20) return list[0];
+// //     else if(a > 20 && a <= 30)  return list[1];
+// //     else if(a > 30 && a <= 40)  return list[2];
+// //     else if(a > 40 && a <= 50)  return list[3];
+// //     else if(a > 50 && a <= 60)  return list[4];
+// //     else {
+
+// //         return new Promise((resolve, reject) => {
+
+// //             setTimeout(() => {
+// //                 resolve(list[5]);
+// //             }, 5000);
+// //         });
+       
+// //     }
+    
+// // }
+
+// // async function run() {
+// //     let javob = await maslahatBering(25);
+// //     console.log(javob);
+// //     javob = await maslahatBering(70);
+// //     console.log(javob);
+// //     javob = await maslahatBering(41);
+// //     console.log(javob);
+// // }
+// //  run();
+
+
+
+
+// // console.log("Jack Ma maslaxatlari");
+// // const list = [
+// //     'yashi talaba buling',  //0-20
+// //     'tugri boshliq tanlang va kupro xato qiling',  //20-30
+// //     'uzingizga ishlashni boshlang',  //30-40
+// //     'siz kuchli bulgan narsalarni qiling', //40-50
+// //     'yoshlarga investitsia qiling',  //50-60
+// //     'endi dam oling, foydasi yuq endi',  //60
+// // ];
+
+
+// // function maslahatBering(a, callback) {
+// //     if(typeof a !== 'number') callback("insert a number", null);
+// //     else if(a <= 20) callback(null, list[0]);
+// //     else if(a > 20 && a <= 30) callback(null,list[1]);
+// //     else if(a > 30 && a <= 40) callback(null,list[2]);
+// //     else if(a > 40 && a <= 50) callback(null,list[3]);
+// //     else if(a > 50 && a <= 60) callback(null,list[4]);
+// //     else {
+// //         setInterval(function() {
+// //             callback(null, list[5]);
+// //         }, 1000);
+    
+// //     }
+    
+// // }
+
+// // console.log('passed here 0');
+// // maslahatBering(70, (err, data) =>{
+// //     if (err) console.log("ERROR:", err);
+// //     else {
+// //         console.log(data);
+// //     }
+// // });
+// // console.log("passed here 1");
+
+
+
+
+
+
+
+
+
+
