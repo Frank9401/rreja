1b0491d6ec552fe1dc0c55b3b8584e678b48a548
Frank9401 <faxriddin0115@gmail.com>
2023-05-11 14:37:56 +0900

    feat: train callback hamda asyn functions

--- server.js
+++ server.js
@@ -18,33 +18,31 @@ app.use(express.static("public"));
 app.use(express.json());// kirib kelyatgan json formatdagi data ni objectga ogiradi
 app.use(express.urlencoded({extended:true})); //html forumdan request qiladi 
 
-//2 Session
+//2 Session 
 
-
+ 
 //3 BSSR -backend serverside render  VIEWS ga bogliq kodlar
 app.set("views", "views");
 app.set("view engine","ejs");
 
-//4 routerlarga moljallangan  ,Routin codlar
-
+//4 routerlarga moljallangan  ,Routin codlar 
+   
 app.post("/create-item",(req,res)=>{
     // console.log("")
     
 });
 
-app.get('/author', (req, res) => {
-    res.render("author", {user:user});
-})
-
+// app.get('/author', (req, res) => {
+//     res.render("author", {user:user});
+// })
+ 
 app.get("/", function(req,res){
     res.render('harid')
 })
-
-
+  
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT , function(){
     console.log(`The server is running succesfully on port : ${PORT}`)
 })
-
