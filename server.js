78808800c16f1835e5595cb742b012ed5ce5af64
Frank9401 <faxriddin0115@gmail.com>
2023-05-13 21:10:01 +0900

    feat:task-d ,MongoDB connection

--- server.js
+++ server.js
@@ -1,48 +1,28 @@
-console.log("Web Server boshlash");
-const express = require("express");
-const app = express() ;
-const http =require("http")
-const fs = require("fs");
+const http = require("http")
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err,data) => {
-    if (err) {
-        console.log("ERROR:", err);
-    } else {
-        user =JSON.parse(data)
-    }
-});  
+const mongodb = require("mongodb");
 
-// 1 express ga kirib kelyatgan malumotlarga bogliq bolgan kodlar yoziladi. KIRISH kodlari
-app.use(express.static("public"));
-app.use(express.json());// kirib kelyatgan json formatdagi data ni objectga ogiradi
-app.use(express.urlencoded({extended:true})); //html forumdan request qiladi 
+let db;
+const connectionString = 
+"mongodb+srv://frank:P0zB65ZNhct0p7rF@cluster0.fzkk7bm.mongodb.net/"
 
-//2 Session  
-
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTropology: true,
+}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log ("MongoDb connection succeed");
+        module.exports = client;
+        const app = require("./app");
+      
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT , function(){
+    console.log(
+        `The server is running succesfully on port : ${PORT}, http://localhost:${PORT}`
+        );
+    });
+}
+} );
  
-//3 BSSR -backend serverside render  VIEWS ga bogliq kodlar
-app.set("views", "views");
-app.set("view engine","ejs");
- 
-//4 routerlarga moljallangan  ,Routin codlar 
-   
-app.post("/create-item",(req,res)=>{
-    // console.log("") 
-    
-}); 
-
-app.get('/author', (req, res) => {
-    res.render("author", {user:user});
-})
- 
-app.get("/", function(req,res){
-    res.render('reja')
-})
-  
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT , function(){
-    console.log(`The server is running succesfully on port : ${PORT}, http://localhost:${PORT}`);
-});
