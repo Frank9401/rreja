804aa789445df8e1058a168c262d7d9c86549a8a
Frank9401 <faxriddin0115@gmail.com>
2023-05-16 12:56:46 +0900

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -36,12 +36,16 @@ app.post("/create-item", (req, res) => {
     // console.log(req.body);
     const new_reja =req.body.reja;
     db.collection("plans").insertOne({reja:new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end('something went wrong');
-        } else {
-            res.end('successfully added');
-        }
+        console.log(data.ops);
+        res.json(data.ops[0]);
+
+
+        // if(err) {
+        //     console.log(err);
+        //     res.end('something went wrong');
+        // } else {
+        //     res.end('successfully added');
+        // }
     });
   
     // res.end("success");
@@ -60,7 +64,7 @@ app.get("/",function(req,res){
             res.end("something went wrong");
             
         } else {
-            console.log(data);
+            // console.log(data);
             res.render("reja", {items: data}); 
         }
     });
