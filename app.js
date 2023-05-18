643ce11be5b62ee55628502229ace8672354ec9b
Frank9401 <faxriddin0115@gmail.com>
2023-05-18 15:42:23 +0900

    feat: Reja loyihamizning yakuniy bosqich logic

--- app.js
+++ app.js
@@ -124,13 +124,38 @@ app.post("/delete-item", (req, res) => {
     // console.log(id);
     // res.end("done");
 
-    db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err,data){
+    db.collection("plans").deleteOne(
+        {_id: new mongodb.ObjectId(id)},
+         function(err,data){
         res.json({state: "success"});
     })
 
 });
 
 
+app.post("/edit-item", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    db.collection("plans").findOneAndUpdate(
+        {_id: new mongodb.ObjectId(data.id)},
+        {$set: {reja: data.new_input} },
+         function(err, data) {
+            res.json({state: "success"});
+         }
+    );
+    // res.end("done");
+});
+
+
+app.post("/delete-all", (req, res) => {
+    if(req.body.delete_all){
+        db.collection("plans").deleteMany(function() {
+            res.json({state: "hamma rejalar uchirildi"});
+        });
+    }
+});
+
+ 
 
 app.get("/",function(req,res){
     console.log("user entered /")
