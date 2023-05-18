5354d732d1868652fcc2aafd0ab757f7c192b23d
Frank9401 <faxriddin0115@gmail.com>
2023-05-18 14:54:51 +0900

    feat: delete

--- app.js
+++ app.js
@@ -1,19 +1,96 @@
+// // console.log("Web Server boshlash");
+// // const express = require("express");
+// // const app = express() ;
+// // const fs =require("fs");
+
+// // let user;
+// // fs.readFile("database/user.json","utf8",(err,data)=>{
+// //     if(err){
+// //         console.log("ERROR: ", err);
+// //     }else{
+// //         user =JSON.parse(data);
+// //     }
+// // });
+
+// // //Mongo DB chaqirish
+// // const db = require("./server").db();
+
+
+
+// // // 1 express ga kirib kelyatgan malumotlarga bogliq bolgan kodlar yoziladi. KIRISH kodlari
+// // app.use(express.static("public"));
+// // app.use(express.json());// kirib kelyatgan json formatdagi data ni objectga ogiradi
+// // app.use(express.urlencoded({extended:true})); //html forumdan request qiladi 
+
+// // //2 Session
+
+// // //3 BSSR -backend serverside render  VIEWS ga bogliq kodlar
+// // app.set("views", "views");
+// // app.set("view engine","ejs");
+
+// // //4 routerlarga moljallangan  ,Routin codlar
+
+
+// // app.post("/create-item", (req, res) => {
+// //     console.log('user entered /create-item');
+// //     // console.log(req.body);
+// //     const new_reja =req.body.reja;
+// //     db.collection("plans").insertOne({reja:new_reja}, (err, data) => {
+// //         console.log(data.ops);
+// //         res.json(data.ops[0]);
+
+
+// //         // if(err) {
+// //         //     console.log(err);
+// //         //     res.end('something went wrong');
+// //         // } else {
+// //         //     res.end('successfully added');
+// //         // }
+// //     });
+  
+// //     // res.end("success");
+// //     // TODO: code with db here
+// // });
+
+// // // app.get("/author",function(req,res){
+// // //     res.render("author",{user:user})
+// // // });
+
+// // app.get("/",function(req,res){
+// //     console.log('user entered /');
+// //     db.collection("plans").find().toArray((err, data) =>{
+// //         if(err) {
+// //             console.log(err);
+// //             res.end("something went wrong");
+            
+// //         } else {
+// //             // console.log(data);
+// //             res.render("reja", {items: data}); 
+// //         }
+// //     });
+// //     // res.render("reja")
+// // });
+  
+
+// module.exports = app;
 console.log("Web Server boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express() ;
-const fs =require("fs");
+// const fs =require("fs");
 
-let user;
-fs.readFile("database/user.json","utf8",(err,data)=>{
-    if(err){
-        console.log("ERROR: ", err);
-    }else{
-        user =JSON.parse(data);
-    }
-});
+// let user;
+// fs.readFile("database/user.json","utf8",(err,data)=>{
+//     if(err){
+//         console.log("ERROR: ", err);
+//     }else{
+//         user =JSON.parse(data);
+//     }
+// });
 
 //Mongo DB chaqirish
 const db = require("./server").db();
+const mongodb =require("mongodb");
 
 
 
@@ -29,47 +106,44 @@ app.set("views", "views");
 app.set("view engine","ejs");
 
 //4 routerlarga moljallangan  ,Routin codlar
-
-
-app.post("/create-item", (req, res) => {
-    console.log('user entered /create-item');
-    // console.log(req.body);
-    const new_reja =req.body.reja;
-    db.collection("plans").insertOne({reja:new_reja}, (err, data) => {
-        console.log(data.ops);
-        res.json(data.ops[0]);
-
-
-        // if(err) {
-        //     console.log(err);
-        //     res.end('something went wrong');
-        // } else {
-        //     res.end('successfully added');
-        // }
+app.post("/create-item",(req,res)=>{
+    console.log("user entered /create-item");
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja:new_reja},(err,data)=>{
+     console.log(data.ops);
+     res.json(data.ops[0]);
     });
-  
-    // res.end("success");
-    // TODO: code with db here
 });
 
-app.get("/author",function(req,res){
-    res.render("author",{user:user})
+// app.get("/author",function(req,res){
+//     res.render("author",{user:user})
+// });
+
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    // console.log(id);
+    // res.end("done");
+
+    db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err,data){
+        res.json({state: "success"});
+    })
+
 });
+
+
 
 app.get("/",function(req,res){
-    console.log('user entered /');
-    db.collection("plans").find().toArray((err, data) =>{
-        if(err) {
+    console.log("user entered /")
+    db.collection("plans").find().toArray((err,data)=>{
+        if(err){
             console.log(err);
-            res.end("something went wrong");
-            
-        } else {
+            res.end('something went wrong');
+        }else{
             // console.log(data);
-            res.render("reja", {items: data}); 
+            res.render("reja",{items:data});
         }
-    });
-    // res.render("reja")
+    })
 });
-  
+
 
 module.exports = app;
